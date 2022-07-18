d11f29f98ebd4ab21091e65f19163fa54a3e3037
Avinava Basu <avinava@mycelium.ventures>
2022-07-18 17:58:43 +0530

    Stream-lined essential outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,34 @@
-output "instances_details" {
-  description = "List of all details for compute instances"
-  value       = google_compute_instance.this.*
+output "eth_fm_instance_name" {
+  description = "The name of the VM instance holding the node"
+  value = module.chainlinknode.instances_details[0].name
 }
 
-output "static_internal_ip" {
-  description = "List of all details for the internal IP"
-  value       = module.address.*
+output "eth_fm_disk_name" {
+  description = "The boot disk name of the node"
+  value = module.chainlinknode.instances_details[0].boot_disk[0].device_name
+}
+
+output "eth_fm_disk_size" {
+  description = "The boot disk size of the node"
+  value = module.chainlinknode.instances_details[0].boot_disk[0].initialize_params[0].size
+}
+
+output "eth_fm_gcs_startup_script" {
+  description = "The private bucket location for the startup script of the node"
+  value = module.chainlinknode.instances_details[0].metadata.startup-script-url
+}
+
+output "eth_fm_external_ip_addresses" {
+  description = "The public IP address of the node"
+  value = module.chainlinknode.instances_details[0].network_interface[0].access_config[0].nat_ip
+}
+
+output "eth_fm_internal_ip_addresses" {
+  description = "The internal static IP address of the node"
+  value = module.chainlinknode.static_internal_ip[0].addresses[0]
+}
+
+output "eth_fm_internal_ip_names" {
+  description = "The internal static IP names of the node"
+  value = module.chainlinknode.static_internal_ip[0].names[0]
 }
