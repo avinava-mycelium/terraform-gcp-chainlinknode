b73ae6d1f712bce60143c99d2704f7d4bc29d6b7
Avinava Basu <avinava@mycelium.ventures>
2022-07-18 20:03:48 +0530

    Modifying readme and adding description

--- main.tf
+++ main.tf
@@ -3,22 +3,23 @@ resource "google_compute_instance" "this" {
   machine_type = var.machine_type
   zone         = var.zone
 
-  tags = [var.network_tag]
+  tags = [var.network_tag] // firewall rules implemeted via network tags
 
   boot_disk {
     device_name = "pd-${var.node_source}-${var.node_name}-${var.node_type}-${var.node_count}"
     initialize_params {
       size = var.boot_disk_size
       type = var.boot_disk_type
-      image = data.google_compute_image.cos_image.self_link
+      image = data.google_compute_image.cos_image.self_link // dynamic allocation of latest stable OS image
     }
   }
 
   network_interface {
     network = var.network
     subnetwork = var.subnetwork
-    network_ip = module.address.addresses[0]
+    network_ip = module.address.addresses[0] // dynamic allocation of static internal IP
     access_config {
+      // kept blank as no specific need for external IP
     }
   }
 
@@ -26,11 +27,11 @@ resource "google_compute_instance" "this" {
     node = var.node_name
     type = var.node_type
     source = var.node_source
-    startup-script-url = "${var.startup_script_prefix}${var.node_name}-${var.node_type}-${var.startup_script_suffix}"
+    startup-script-url = "${var.startup_script_prefix}${var.node_name}-${var.node_type}-${var.startup_script_suffix}" // deployment script for application stored in a private bucket
   }
 
   service_account {
-    email = data.google_secret_manager_secret_version.sa.secret_data
+    email = data.google_secret_manager_secret_version.sa.secret_data // Service account details pulled from secrets manager
     scopes = [var.service_account_scope]
   }
 }
