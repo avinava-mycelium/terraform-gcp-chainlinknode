cdbaa626a70802fde6c8a5854ab942a11683bdc9
Avinava Basu <avinava@mycelium.ventures>
2022-07-21 04:11:08 +0530

    External Ip allcoation changes

--- private_address.tf
+++ private_address.tf
@@ -8,3 +8,13 @@ module "address" {
   address_type = var.address_type
   network_tier = var.address_tier
 }
+
+module "ext_address" {
+  source       = "terraform-google-modules/address/google"
+  version      = "3.0.0"
+  project_id   = var.project_id
+  region       = var.region
+  names        = ["ip-ext-${var.node_source}-${var.node_name}-${var.node_type}-${var.node_count}"]
+  address_type = "EXTERNAL"
+  network_tier = var.address_tier
+}
